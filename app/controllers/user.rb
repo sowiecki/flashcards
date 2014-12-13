f47591eacf9198086ae3f5059de47d91ab876219
Bane Srdjevic <banesrdjevic@gmail.com>
2014-12-13 15:15:40 -0600

    trying to fix conflicts

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,13 +1,7 @@
 get '/user/:id' do
   @user = User.find_by(email: params[:email])
-<<<<<<< HEAD
   # @decks = Deck.where(creator_id: @user.id) -- use if only show decks they created
   @decks = Deck.all
   session[:user] = @user
   erb :'user/index'
-=======
-  @decks = Deck.where(creator_id: @user.id)
-  session[:user] = @user
-  erb :'user/show'
->>>>>>> d3e3725abdf66d5e471638dbb2ee720b1e5ef27d
 end
