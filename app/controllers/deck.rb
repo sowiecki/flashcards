fef8df33a640eed2208de7c35a2c7b277ba84d41
nase00@gmail.com <nase00@gmail.com>
2014-12-14 16:27:13 -0600

    Minimized data stored in session

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,8 +1,13 @@
 get '/deck/:id' do
   @deck = Deck.find(params[:id])
-
-  @cards = Card.where(deck_id: params[:id]).shuffle
-  session[:cards] = @cards
-  @card = session[:cards].pop
+  @cards = Card.where(deck_id: params[:id])
+  @card_ids = []
+  @cards.each do |card|
+    @card_ids << card.id
+  end
+  session[:card_ids] = @card_ids
+  # @card = Card.where(id: session[:card_ids])
+  card_id = session[:card_ids].first
+  @card = Card.where(id: card_id)
   erb :'deck/show'
 end
