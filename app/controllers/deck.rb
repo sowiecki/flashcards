4f7a30f9fd6a9aaf897ebefe78a732b0622d7a49
nase00@gmail.com <nase00@gmail.com>
2014-12-14 20:15:11 -0600

    minor changes to views and gemfile

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,6 +1,6 @@
 get '/deck/:id' do
   @deck = Deck.find(params[:id])
-  @cards = Card.where(deck_id: params[:id]).shuffle
+  @cards = Card.where(deck_id: params[:id])
   @card_ids = []
   @cards.each do |card|
     @card_ids << card.id
@@ -8,6 +8,6 @@ get '/deck/:id' do
   session[:card_ids] = @card_ids # Only store the card's IDs in the session!
   # @card = Card.where(id: session[:card_ids])
   @card_id = session[:card_ids].first
-  @card = Card.where(id: @card_id, deck_id: params[:id]).first
+  @card = Card.where(id: @card_id, deck_id: params[:id]).sample
   erb :'deck/show'
 end
